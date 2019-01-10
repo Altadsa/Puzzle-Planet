82e32f688e028cb98f2d521cf74188e403a8ef51
Adam Coyle <adam.coyle@fhnw.ch>
2019-01-10 12:32:11 +0100

    Basic Functionality but the code is horrifying to look at.

---
+++ Assets/PuzzleGame.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class PuzzleGame : MonoBehaviour
+{
+    public Texture2D texture;
+    Sprite[] puzzleImage;
+    public GameObject piecePrefab;
+
+    GraphicRaycaster raycaster;
+
+    PuzzlePiece selectedPiece;
+
+    public int piecesLeftToSolve;
+
+    private void Awake()
+    {
+        raycaster = GetComponent<GraphicRaycaster>();
+    }
+
+    void Start()
+    {
+        SetupPuzzle();
+        ShufflePuzzle();
+    }
+
+    private void SetupPuzzle()
+    {
+        int pos = 0;
+        puzzleImage = Resources.LoadAll<Sprite>(texture.name);
+        foreach (Sprite puzzleImg in puzzleImage)
+        {
+            GameObject nPiece = Instantiate(piecePrefab, transform);
+            nPiece.transform.SetSiblingIndex(pos);
+            nPiece.GetComponent<PuzzlePiece>().pieceImage = puzzleImg;
+            nPiece.GetComponent<PuzzlePiece>().startPos = pos;
+            pos++;
+        }
+        piecesLeftToSolve = transform.childCount;
+    }
+
+    private void ShufflePuzzle()
+    {
+
+        foreach (Transform piece in transform)
+        {
+            int rNum = Random.Range(0, transform.childCount - 1);
+            piece.SetSiblingIndex(rNum);       
+        }
+
+        foreach (Transform piece in transform)
+        {
+            piece.GetComponent<PuzzlePiece>().currentPos = piece.transform.GetSiblingIndex();
+            piece.GetComponent<PuzzlePiece>().CheckIfInPosition();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckIfTouchingPiece();
+        }
+        if (piecesLeftToSolve == 0)
+        {
+            Destroy(gameObject);
+        }
+        Debug.Log("Solves left:" + piecesLeftToSolve);
+    }
+
+    private void CheckIfTouchingPiece()
+    {
+
+        if (selectedPiece)
+        {
+            PuzzlePiece pieceToSwap = RaycastForPiece();
+            if (pieceToSwap == selectedPiece)
+            {
+                selectedPiece.UnmarkSelected();
+                selectedPiece = null;
+                return;
+            }
+            else
+            {
+                if (selectedPiece && pieceToSwap)
+                {
+                    SwapPieces(selectedPiece, pieceToSwap);
+                    selectedPiece.UnmarkSelected();
+                    selectedPiece = null;
+                    pieceToSwap = null;
+                }
+            }
+        }
+        else
+        {
+            if (RaycastForPiece().canBeSelected)
+            {
+                selectedPiece = RaycastForPiece();
+                selectedPiece.MarkSelected();
+            }
+        }
+    }
+
+    private void SwapPieces(PuzzlePiece a, PuzzlePiece b)
+    {
+        int temp = a.transform.GetSiblingIndex();
+        a.transform.SetSiblingIndex(b.transform.GetSiblingIndex());
+        b.transform.SetSiblingIndex(temp);
+        a.CheckIfInPosition();
+        b.CheckIfInPosition();
+    }
+ 
+
+    PuzzlePiece RaycastForPiece()
+    {
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        List<RaycastResult> results = new List<RaycastResult>();
+        pointerData.position = Input.mousePosition;
+        raycaster.Raycast(pointerData, results);
+
+        GameObject selected = results[0].gameObject;
+        PuzzlePiece puzzlePiece = selected.GetComponent<PuzzlePiece>();
+        if (puzzlePiece != selectedPiece)
+        {
+            return puzzlePiece;
+        }
+        return selectedPiece;
+    }
+}
---
+++ Assets/PuzzlePiece.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    public Sprite pieceImage;
+
+    public int startPos, currentPos;
+
+    [HideInInspector]
+    public bool isSeleted;
+
+    [HideInInspector]
+    public bool canBeSelected;
+
+    private void Start()
+    {
+        GetComponent<Image>().sprite = pieceImage;
+    }
+
+    public void MarkSelected()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        isSeleted = true;
+    }
+
+    public void UnmarkSelected()
+    {
+        transform.localScale = Vector3.one;
+        isSeleted = false;
+    }
+
+    public void CheckIfInPosition()
+    {
+        currentPos = transform.GetSiblingIndex();
+        Debug.Log(string.Format("Start Pos: {0} Current Pos: {1}", startPos, currentPos));
+        if (currentPos == startPos)
+        {
+            canBeSelected = false;
+            GetComponentInParent<PuzzleGame>().piecesLeftToSolve--;
+        }
+        else
+        {
+            canBeSelected = true;
+        }
+    }
+
+}
